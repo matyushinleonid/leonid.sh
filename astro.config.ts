6d0229b75d2af9894b1222ecbaece50fc5796a3a
matyushinleonid <matyushinleonid@users.noreply.github.com>
2026-08-15 16:01:36 +0300

    Update domain (#5)

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,7 @@ import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://home.leonid.sh",
+  site: "https://leonid.sh",
   output: "static",
   trailingSlash: "always",
   integrations: [sitemap()],
